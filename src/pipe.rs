e09d700359b8e36719da6627c135080021058089
Amirabbas Fazelinia <utilyre@proton.me>
2023-05-19 23:20:52 +0330

    feat(pipe): implement randomly generated pipe obstacles

--- src/pipe.rs
+++ src/pipe.rs
@@ -1,8 +1,10 @@
 use crate::{RESOLUTION, SCALE};
 use bevy::prelude::*;
+use rand::Rng;
 use std::time::Duration;
 
-const PIPE_SIZE: (f32, f32) = (16.0, 16.0);
+const SPRITE_SIZE: (f32, f32) = (16.0, 16.0);
+const NUMBER_OF_GRIDS: u32 = (RESOLUTION.1 / (SCALE * SPRITE_SIZE.1)) as u32;
 
 const SPAWN_INTERVAL: u64 = 2000;
 const SPEED: f32 = 200.0;
@@ -37,14 +39,25 @@ fn spawner(
         return;
     }
 
-    commands
-        .spawn(SpriteBundle {
-            texture: asset_server.load("pipe.png"),
-            transform: Transform::from_xyz(RESOLUTION.0 / 2.0 + SCALE * PIPE_SIZE.0, 0.0, 0.0)
+    let empty_idx = rand::thread_rng().gen_range(0..NUMBER_OF_GRIDS);
+    for i in 0..NUMBER_OF_GRIDS {
+        if i == empty_idx {
+            continue;
+        }
+
+        commands
+            .spawn(SpriteBundle {
+                texture: asset_server.load("pipe.png"),
+                transform: Transform::from_xyz(
+                    RESOLUTION.0 / 2.0 + SCALE * SPRITE_SIZE.0,
+                    -RESOLUTION.1 / 2.0 + SCALE * ((i as f32 + 0.5) * SPRITE_SIZE.1),
+                    0.0,
+                )
                 .with_scale(Vec3::new(SCALE, SCALE, 1.0)),
-            ..default()
-        })
-        .insert(Pipe);
+                ..default()
+            })
+            .insert(Pipe);
+    }
 }
 
 fn movement(mut pipes: Query<&mut Transform, With<Pipe>>, time: Res<Time>) {
